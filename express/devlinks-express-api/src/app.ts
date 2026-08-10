8594593b6418a22515a5f8dfaf45050972197fbd
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-08-10 14:38:09 -0300

    feat(devlinks-api): add request logger middleware and link router setup
    - Created logger.middleware.ts measuring HTTP request duration and logging method, path, and execution time on response finish.
    - Created link.routes.ts expressing basic link router with initial GET route.
    - Registered loggerMiddleware and router in app.ts.

--- express/devlinks-express-api/src/app.ts
+++ express/devlinks-express-api/src/app.ts
@@ -1,3 +1,9 @@
 import express from "express";
+import { router } from "./routes/link.routes.js";
+import { loggerMiddleware } from "./middlewares/logger.middleware.js";
 
 export const app = express();
+
+app.use(loggerMiddleware);
+
+app.use(router);
---
+++ express/devlinks-express-api/src/middlewares/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+export function loggerMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const requestTime = performance.now();
+
+  res.on("finish", () => {
+    const responseTime = performance.now() - requestTime;
+
+    console.log(`${req.method} ${req.path} (${responseTime.toFixed(2)} ms)`);
+  });
+
+  next();
+}
