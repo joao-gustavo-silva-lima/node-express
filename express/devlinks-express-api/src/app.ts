2da002132680f5f6c64f19be3f30737293e5355b
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-08-17 10:13:49 -0300

    feat(devlinks-api): add cors middleware to enable cross-origin requests
    - Installed cors package alongside @types/cors development dependency.
    - Integrated cors middleware into app instance to allow requests from frontend client origins.

--- express/devlinks-express-api/src/app.ts
+++ express/devlinks-express-api/src/app.ts
@@ -1,9 +1,12 @@
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { loggerMiddleware } from "./middlewares/logger.middleware.js";
 import { router } from "./routes/link.routes.js";
+import cors from "cors";
 import express from "express";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
