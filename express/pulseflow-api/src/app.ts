88f5638eec555aad4d9e4a7e775f0cadf036657d
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-09-14 12:41:57 -0300

    feat: add CORS support
    - Added CORS middleware to enable cross-origin requests

--- express/pulseflow-api/src/app.ts
+++ express/pulseflow-api/src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { router } from "./router/router.js";
 import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
 import { loggerMiddleware } from "./middlewares/logger.middleware.js";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
